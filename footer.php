ee7dafef976ab85cc0480eb0899ceb472a94ba9e
itkomuna <itkomunanovisad@gmail.com>
2024-09-16 21:35:05 +0200

    header i footer odovojeni na sve stranice pomocu include funkcije

---
+++ footer.php
@@ -0,0 +1,34 @@
+<!-- footer -->
+<footer class="py-5 text-white">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-4">
+                    <h3 class="d-inline-block">O nama</h3>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo aut veniam voluptatibus officiis sapiente dolorum eos! Ullam quam quasi tempore.</p>
+                </div>
+                <div class="col-md-4">
+                    <h3 class="d-inline-block">Kontakt</h3>
+                    <ul class="list-unstyled contact">
+                        <li class="mb-2"><i class="bi bi-pin-map-fill"></i> Glavna ulica 34, Apatin</li>
+                        <li class="mb-2"><i class="bi bi-telephone-fill"></i> 025 774 403</li>
+                        <li><i class="bi bi-envelope-fill"></i> office@mintmedia.rs</li>
+                    </ul>
+                </div>
+                <div class="col-md-4">
+                    <h3 class="d-inline-block">Povezimo se</h3>
+                    <ul class="list-unstyled socials">
+                        <li class="d-inline-block"><a href=""><i class="bi bi-facebook"></i></a></li>
+                        <li class="d-inline-block"><a href=""><i class="bi bi-instagram"></i></a></li>
+                        <li class="d-inline-block"><a href=""><i class="bi bi-linkedin"></i></a></li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </footer>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz"
+        crossorigin="anonymous"></script>
+</body>
+
+</html>
