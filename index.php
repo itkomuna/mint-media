b74a9ec91f54939c8d9a690e2e568ce1cc56955d
itkomuna <itkomunanovisad@gmail.com>
2024-09-18 20:31:37 +0200

    home gotov

--- index.php
+++ index.php
@@ -1,10 +1,52 @@
 <?php include 'header.php'; ?>
 
-    <!-- main header -->
-    <header class="mainHeader">
-        <img class="w-100" src="img/header.jpg" alt="">
-    </header>
+<!-- main header -->
+<header class="mainHeader">
+    <img class="w-100" src="img/header.jpg" alt="">
+</header>
+
+<!-- baner -->
+<section class="baner container text-center p-5 text-white position-relative">
+    <h1>Najbolja solucija za rast vaseg bisnisa</h1>
+    <p class="lead">Kada želite promociju svog brenda i usluge onda je najsvrsishodnije rešenje da imate sve na jednom
+        mestu: facebook i google, reklame, kopirajt, SEO...</p>
+</section>
+
+<!-- usluge -->
+<section class="usluge container py-5">
+    <h2 class="text-center display-3">Usluge</h2>
+    <div class="row">
+        <article class="col-md-4">
+            <div class="card">
+                <img src="img/mint-socijalne-mreze.jpg" class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title">Drustvene mreze</h5>
+                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                    <a href="#" class="btn btn-primary">Procit vise</a>
+                </div>
+            </div>
+        </article>
+        <article class="col-md-4">
+            <div class="card">
+                <img src="img/mint_seo.jpg" class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title">SEO</h5>
+                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                    <a href="#" class="btn btn-primary">Procit vise</a>
+                </div>
+            </div>
+        </article>
+        <article class="col-md-4">
+            <div class="card">
+                <img src="img/mint-google-oglasi.jpg" class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title">Google oglasi</h5>
+                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                    <a href="#" class="btn btn-primary">Procit vise</a>
+                </div>
+            </div>
+        </article>
+    </div>
+</section>
 
 <?php include 'footer.php'; ?>
-
-    
