2a1007211be7c061368cbf48aef7388a8f7b11c6
itkomuna <itkomunanovisad@gmail.com>
2024-10-07 20:53:06 +0200

    blog listing

--- functions.php
+++ functions.php
@@ -11,6 +11,8 @@ function ispisi_ime_sajta() {
     echo $ime_sajta;
 }
 
+//    USLUGE LISTING     //
+
 function ucitaj_usluge_text_file() {
     return file_get_contents('baza/usluge.txt');
 }
@@ -66,4 +68,71 @@ function izlistaj_sve_usluge() {
     $usluge_podaci = normalizacija_podataka($usluge_podaci);
     //debagovanje($usluge_podaci);
     return $usluge_podaci;
+}
+
+
+//    BlOG LISTING     //
+function ocitaj_blog_text_fajl() {
+    return file_get_contents('baza/blog.txt');
+}
+
+function normalizacija_blog_podataka($blog_podaci) {
+    $prelomi_blog = explode('++++++++++++++', $blog_podaci);
+    //debagovanje($prelomi_blog); 
+    unset($prelomi_blog[0]);
+    //debagovanje($prelomi_blog); 
+
+    $novi_blog_red_formatiran = [];
+    foreach($prelomi_blog as $blog_novi_red) {
+        //debagovanje($blog_novi_red);
+        $novi_blog_red_razbijen = explode(PHP_EOL, $blog_novi_red);
+        //debagovanje($novi_blog_red_razbijen);
+        $novi_blog_red_razbijen = array_filter($novi_blog_red_razbijen);
+        //debagovanje($novi_blog_red_razbijen);
+        // debagovanje(normalizacija_redova_blog($novi_blog_red_razbijen[1]));
+        // debagovanje(normalizacija_redova_blog($novi_blog_red_razbijen[2]));
+        // debagovanje(normalizacija_redova_blog($novi_blog_red_razbijen[3]));
+        // debagovanje(normalizacija_redova_blog($novi_blog_red_razbijen[4]));
+        // debagovanje(normalizacija_redova_blog($novi_blog_red_razbijen[5]));
+        // debagovanje(normalizacija_redova_blog($novi_blog_red_razbijen[6]));
+        $blog_red_id = normalizacija_redova_blog($novi_blog_red_razbijen[1]);
+        $blog_red_slika = normalizacija_redova_blog($novi_blog_red_razbijen[2]);
+        $blog_red_naslov = normalizacija_redova_blog($novi_blog_red_razbijen[3]);
+        $blog_red_datum = normalizacija_redova_blog($novi_blog_red_razbijen[4]);
+        $blog_red_admin = normalizacija_redova_blog($novi_blog_red_razbijen[5]);
+        $blog_red_tekst = normalizacija_redova_blog($novi_blog_red_razbijen[6]);
+        // debagovanje($blog_red_id);
+        // debagovanje($blog_red_slika);
+        // debagovanje($blog_red_naslov);
+        // debagovanje($blog_red_datum);
+        // debagovanje($blog_red_admin);
+        // debagovanje($blog_red_tekst);
+
+        $blog_red_podaci = [
+            'id' => $blog_red_id,
+            'slika' => $blog_red_slika,
+            'naslov' => $blog_red_naslov,
+            'datum' => $blog_red_datum,
+            'admin' => $blog_red_admin,
+            'tekst' => $blog_red_tekst
+        ];
+        //debagovanje($blog_red_podaci);
+        $novi_blog_red_formatiran[] = $blog_red_podaci;
+    }
+    //debagovanje($novi_blog_red_formatiran);
+    return $novi_blog_red_formatiran;
+}
+
+function normalizacija_redova_blog($blog_podaci_red) {
+    $normalizuj_blog_podatke = ['ID:', 'Slika:', 'Naslov:', 'Datum:', 'Admin:', 'Tekst:'];
+    $blog_red = str_replace($normalizuj_blog_podatke, "", $blog_podaci_red);
+    $blog_red = trim($blog_red);
+    return $blog_red;
+}
+
+function izlistaj_sve_blogove() {
+    $blog_podaci = ocitaj_blog_text_fajl();
+    $blog_podaci = normalizacija_blog_podataka($blog_podaci);
+    //debagovanje($blog_podaci);
+    return $blog_podaci;
 }
--- template_parts/blog-part.php
+++ template_parts/blog-part.php
@@ -1,40 +1,29 @@
+<?php 
+//debagovanje(ocitaj_blog_text_fajl());
+//normalizacija_blog_podataka(ocitaj_blog_text_fajl());
+$blog_postovi = izlistaj_sve_blogove();
+//debagovanje($blog_postovi);
+?>
+
 <!-- blog -->
 <main class="container">
     <div class="row">
         <section class="col-md-8">
+
+        <?php foreach($blog_postovi as $post) : ?>
             <article>
                 <div class="featured-image">
-                    <img class="w-100" src="img/mint drustvene mreze.jpg" alt="">
+                    <img class="w-100" src="<?php echo $post['slika']; ?>" alt="<?php echo $post['naslov']; ?>">
                 </div>
                 <div class="text position-relative p-5 text-white">
-                    <h3>Drustvene mreze</h3>
-                    <span class="bg-dark text-white d-inline-block py-1 px-3 mb-3">18.09.2024. | Admin</span>
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nobis quaerat a earum numquam sequi voluptate culpa, facilis fugiat quod neque atque similique sunt veritatis. Mollitia totam illum repellat voluptas architecto enim suscipit, voluptatem eius ut excepturi neque, quia voluptatibus harum velit ullam atque officia quae similique beatae officiis ea expedita.</p>
+                    <h3><?php echo $post['naslov']; ?></h3>
+                    <span class="bg-dark text-white d-inline-block py-1 px-3 mb-3"><?php echo $post['datum']; ?> | <?php echo $post['admin']; ?></span>
+                    <p><?php echo $post['tekst']; ?></p>
                     <a href="" class="btn btn-light">Ceo tekst</a>
                 </div>
             </article>
-            <article>
-                <div class="featured-image">
-                    <img class="w-100" src="img/mint_seo.jpg" alt="">
-                </div>
-                <div class="text position-relative p-5 text-white">
-                    <h3>Search engine optimisation</h3>
-                    <span class="bg-dark text-white d-inline-block py-1 px-3 mb-3">18.09.2024. | Admin</span>
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nobis quaerat a earum numquam sequi voluptate culpa, facilis fugiat quod neque atque similique sunt veritatis. Mollitia totam illum repellat voluptas architecto enim suscipit, voluptatem eius ut excepturi neque, quia voluptatibus harum velit ullam atque officia quae similique beatae officiis ea expedita.</p>
-                    <a href="" class="btn btn-light">Ceo tekst</a>
-                </div>
-            </article>
-            <article>
-                <div class="featured-image">
-                    <img class="w-100" src="img/mint-google-ads.jpg" alt="">
-                </div>
-                <div class="text position-relative p-5 text-white">
-                    <h3>Google ads </h3>
-                    <span class="bg-dark text-white d-inline-block py-1 px-3 mb-3">18.09.2024. | Admin</span>
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nobis quaerat a earum numquam sequi voluptate culpa, facilis fugiat quod neque atque similique sunt veritatis. Mollitia totam illum repellat voluptas architecto enim suscipit, voluptatem eius ut excepturi neque, quia voluptatibus harum velit ullam atque officia quae similique beatae officiis ea expedita.</p>
-                    <a href="" class="btn btn-light">Ceo tekst</a>
-                </div>
-            </article>
+        <?php endforeach; ?>
+            
         </section>
         <aside class="col-md-4">
             <?php include 'template_parts/sidebar.php'; ?>
